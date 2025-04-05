04ecbc6e8d9ffa39706dafda197291e79c1e4851
Vicheslav Der <efirshey@gmail.com>
2025-04-05 16:32:42 +0300

    soltuion

--- task.ps1
+++ task.ps1
@@ -1,1 +1,12 @@
-# Write your code here
+$directoryPath = "$PSScriptRoot\data"
+$resultFilePath = "$PSScriptRoot\result.json"
+
+$result = @()
+
+foreach ($file in Get-ChildItem -Path $directoryPath -Filter *.json -File) {
+    if (Select-String -Path $file.FullName -Pattern "Standard_B2pts_v2" -Quiet) {
+        $result += [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
+    }
+}
+
+$result | ConvertTo-Json | Out-File -FilePath $resultFilePath -Encoding UTF8
